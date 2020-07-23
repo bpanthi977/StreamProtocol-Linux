8fcec86295ab19f1306b4cdb28f86291aefbc76a
Bibek Panthi <bpanthi977@gmail.com>
2020-07-23 19:46:25 +0545

    xdg-mime requires only .desktop file name

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cp ./streamProtocol.desktop ~/.local/share/applications/
-xdg-mime default ~/.local/share/applicationsstreamProtocol.desktop x-scheme-handler/stream+http
+xdg-mime default streamProtocol.desktop x-scheme-handler/stream+http
