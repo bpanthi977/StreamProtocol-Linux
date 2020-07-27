1229eda606f4ac82d7c1e885e1fdad0a9aa6d511
Bibek Panthi <bpanthi977@gmail.com>
2020-07-27 22:15:40 +0545

    added https support

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cp ./streamProtocol.desktop ~/.local/share/applications/
 xdg-mime default streamProtocol.desktop x-scheme-handler/stream+http
+xdg-mime default streamProtocol.desktop x-scheme-handler/stream+https
